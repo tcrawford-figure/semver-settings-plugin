1f479e761c4be9f88951058e0b2f3ccb18223102
Tyler Crawford <tcrawford@figure.com>
2023-08-06 20:52:56 -0400

    More changes

--- src/main/kotlin/com/figure/gradle/semver/ScratchMain.kt
+++ src/main/kotlin/com/figure/gradle/semver/ScratchMain.kt
@@ -1,20 +1,22 @@
 package com.figure.gradle.semver
 
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.revwalk.RevCommit
 import java.io.File
 
 fun main(args: Array<String>) {
     val git = Git.open(File("/Users/tylercrawford/dev/playgrounds/semver-settings-plugin/.git"))
 
-    val currentBranch = git.repository.branch
-    println("Current branch: $currentBranch")
+    val commits: List<RevCommit> = git.log().call().toList()
 
-    val commits = git.log().call().toList()
-
-    val uniqueBranches = commits
+    val index = commits
         .map { git.branchList().setContains(it.name).call() }
-        .flatten()
-        .toSet()
+        .indexOfFirst { it.size > 1 }
 
-    println(uniqueBranches)
+    val counts = commits.take(index).count()
+
+    println(counts)
 }
+
+fun Git.branchesCount(revCommit: RevCommit): Int =
+    branchList().setContains(revCommit.name).call().size
