1aaeacde4e8cdb87962ed4914bbe078686b02585
Tyler Crawford <tcrawford@figure.com>
2024-09-16 21:05:42 -0400

    Revert temp changes

--- src/functionalTest/kotlin/io/github/tcrawford/versioning/specs/UseInitialVersionSpec.kt
+++ src/functionalTest/kotlin/io/github/tcrawford/versioning/specs/UseInitialVersionSpec.kt
@@ -1,6 +1,5 @@
 package io.github.tcrawford.versioning.specs
 
-import io.github.tcrawford.versioning.gradle.build
 import io.github.tcrawford.versioning.kotest.GradleProjectsExtension
 import io.github.tcrawford.versioning.kotest.shouldOnlyHave
 import io.github.tcrawford.versioning.projects.RegularProject
@@ -8,17 +7,16 @@ import io.github.tcrawford.versioning.projects.SettingsProject
 import io.github.tcrawford.versioning.projects.SubprojectProject
 import io.kotest.core.extensions.install
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
 import org.gradle.util.GradleVersion
 
 class UseInitialVersionSpec : FunSpec({
-    // val projects = install(
-    //     GradleProjectsExtension(
-    //         RegularProject(projectName = "regular-project"),
-    //         SettingsProject(projectName = "settings-project"),
-    //         SubprojectProject(projectName = "subproject-project"),
-    //     ),
-    // )
+    val projects = install(
+        GradleProjectsExtension(
+            RegularProject(projectName = "regular-project"),
+            SettingsProject(projectName = "settings-project"),
+            SubprojectProject(projectName = "subproject-project"),
+        ),
+    )
 
     val mainBranch = "main"
     val developmentBranch = "develop"
@@ -27,8 +25,8 @@ class UseInitialVersionSpec : FunSpec({
     context("should use initial version") {
         test("on main branch") {
             // Given
-            val project = RegularProject("reg")
-            project.git {
+            // The default initial value is "0.0.0" which is supplied by the plugin
+            projects.git {
                 initialBranch = mainBranch
                 actions = actions {
                     commit("1 commit on $mainBranch")
@@ -36,67 +34,50 @@ class UseInitialVersionSpec : FunSpec({
             }
 
             // When
-            build(GradleVersion.current(), project.gradleProject.rootDir)
+            projects.build(GradleVersion.current())
 
             // Then
-            project.version shouldBe "0.0.1"
+            projects.versions shouldOnlyHave "0.0.1"
         }
 
-        // test("on main branch") {
-        //     // Given
-        //     // The default initial value is "0.0.0" which is supplied by the plugin
-        //     projects.git {
-        //         initialBranch = mainBranch
-        //         actions = actions {
-        //             commit("1 commit on $mainBranch")
-        //         }
-        //     }
-        //
-        //     // When
-        //     projects.build(GradleVersion.current())
-        //
-        //     // Then
-        //     projects.versions shouldOnlyHave "0.0.1"
-        // }
-        //
-        // test("on development branch") {
-        //     // Given
-        //     // The default initial value is "0.0.0" which is supplied by the plugin
-        //     projects.git {
-        //         initialBranch = mainBranch
-        //         actions = actions {
-        //             commit("1 commit on $mainBranch")
-        //
-        //             checkout(developmentBranch)
-        //             commit("1 commit on $developmentBranch")
-        //         }
-        //     }
-        //
-        //     // When
-        //     projects.build(GradleVersion.current())
-        //
-        //     // Then
-        //     projects.versions shouldOnlyHave "0.0.1-develop.1"
-        // }
-        //
-        // test("on feature branch") {
-        //     // Given
-        //     // The default initial value is "0.0.0" which is supplied by the plugin
-        //     projects.git {
-        //         initialBranch = mainBranch
-        //         actions = actions {
-        //             commit("1 commit on $mainBranch")
-        //
-        //             checkout(featureBranch)
-        //             commit("1 commit on $featureBranch")
-        //         }
-        //     }
-        //
-        //     // When
-        //     projects.build(GradleVersion.current())
-        //
-        //     // Then
-        //     projects.versions shouldOnlyHave "0.0.1-patch-1.1"
-        // }
+        test("on development branch") {
+            // Given
+            // The default initial value is "0.0.0" which is supplied by the plugin
+            projects.git {
+                initialBranch = mainBranch
+                actions = actions {
+                    commit("1 commit on $mainBranch")
+
+                    checkout(developmentBranch)
+                    commit("1 commit on $developmentBranch")
+                }
+            }
+
+            // When
+            projects.build(GradleVersion.current())
+
+            // Then
+            projects.versions shouldOnlyHave "0.0.1-develop.1"
+        }
+
+        test("on feature branch") {
+            // Given
+            // The default initial value is "0.0.0" which is supplied by the plugin
+            projects.git {
+                initialBranch = mainBranch
+                actions = actions {
+                    commit("1 commit on $mainBranch")
+
+                    checkout(featureBranch)
+                    commit("1 commit on $featureBranch")
+                }
+            }
+
+            // When
+            projects.build(GradleVersion.current())
+
+            // Then
+            projects.versions shouldOnlyHave "0.0.1-patch-1.1"
+        }
     }
 })
